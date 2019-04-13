769b2268395baae819fa26f440a4bf01bfdde848
Chris <christopherbernold@icloud.com>
2019-04-13 16:46:35 +0200

    Fixed relationship issues that would cause issues when creating webpage and created a second Migration

--- DAB BlackBoardRemake/BlackBoard/BlackBoard/Models/Enroll.cs
+++ DAB BlackBoardRemake/BlackBoard/BlackBoard/Models/Enroll.cs
@@ -7,12 +7,10 @@ namespace BlackBoard.Models
 {
     public class Enroll
     {
-        public int EnrollId { get; set; }
+        public string CourseId { get; set; }
+        public Course Course { get; set; }
 
-        //Student FK
         public int StudentAuId { get; set; }
-        public Student Student { get; set; }
-
-        public List<Course> Courses { get; set; }
+        public Student Student { get; set; }       
     }
 }
